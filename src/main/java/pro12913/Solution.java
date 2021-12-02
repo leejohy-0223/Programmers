698e00065c7f4fd45464a8599aa3716e5bbff5ef
leejohy-0223 <leejohy@naver.com>
2021-12-02 14:01:10 +0900

    (2021-12-02) ps solve (programmers)

---
+++ src/main/java/pro12913/Solution.java
@@ -0,0 +1,22 @@
+package pro12913;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    int solution(int[][] land) {
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][3]);
+            land[i][1] += Math.max(Math.max(land[i - 1][0], land[i - 1][2]), land[i - 1][3]);
+            land[i][2] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][3]);
+            land[i][3] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][2]);
+        }
+
+        return Arrays.stream(land[land.length - 1]).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
